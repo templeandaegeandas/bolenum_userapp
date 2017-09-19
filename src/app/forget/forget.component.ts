9c27a642c610e192c3dbdbbc5395b2add686d993
Himanshu <himanshu.kumar@oodlestechnologies.com>
2017-09-19 18:58:28 +0530

    integration done with backend code for forget password and reset password

--- src/app/forget/forget.component.ts
+++ src/app/forget/forget.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { ForgetPasswordService } from './forget.password.service';
+import { ToastrService } from 'toastr-ng2';
 
 @Component({
   selector: 'app-forget',
   templateUrl: './forget.component.html',
-  styleUrls: ['./forget.component.css']
+  styleUrls: ['./forget.component.css'] ,
+  providers: [ForgetPasswordService]
 })
 export class ForgetComponent implements OnInit {
-
-  constructor() { }
+email:String
+  constructor(private forgetpasswordService: ForgetPasswordService, private toastrService: ToastrService, private router: Router) {}
 
   ngOnInit() {
+  }
+
+  forgetPassword(form) {
+    if (form.invalid) {
+        return;
+    }
+
+    this.forgetpasswordService.forgetpass(this.email).subscribe(success => {
+      console.log(success);
+      this.toastrService.success(success.message, 'Success!');
+      this.router.navigate(['dashboard']);
+    },error=> {
+       this.toastrService.error(error.json().message, 'Error!');
+    })
   }
 
 }
--- src/app/resetpassword/resetpassword.component.ts
+++ src/app/resetpassword/resetpassword.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes, Router,ActivatedRoute } from '@angular/router';
+import { ResetPassword } from './entity/reset';
+import { ToastrService } from 'toastr-ng2';
+import { ResetPasswordService } from './resetpassword.service';
 
 @Component({
   selector: 'app-resetpassword',
   templateUrl: './resetpassword.component.html',
-  styleUrls: ['./resetpassword.component.css']
+  styleUrls: ['./resetpassword.component.css'] ,
+  providers:[ResetPasswordService]
 })
 export class ResetpasswordComponent implements OnInit {
+  resetPassword = new ResetPassword("","");
+  token : any ;
+  constructor(private resetPasswordService: ResetPasswordService, private toastrService: ToastrService, private router: Router,private activatedRouter:ActivatedRoute)
+  {  }
 
-  constructor() { }
 
   ngOnInit() {
+    this.activatedRouter
+    .queryParams
+    .subscribe(params => {
+        this.token = params['token'];
+    });
+
+  }
+
+  passwordReset(form) {
+    if (form.invalid || this.resetPassword.newPassword!=this.resetPassword.confirmPassword) {
+        return;
+    }
+    console.log(this.resetPassword);
+    this.resetPasswordService.resetPass(this.token,this.resetPassword).subscribe(success => {
+      console.log(success);
+      this.toastrService.success(success.message, 'Success!');
+      this.router.navigate(['login']);
+    },error=> {
+       this.toastrService.error(error.json().message, 'Error!');
+    })
   }
 
 }
