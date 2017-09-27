79d048f8abf625e9c9693ced2d2682bb7a2d0606
swatirahangdale <swati.rahangdale@oodlestechnologies.com>
2017-09-27 10:45:16 +0530

    profile page  change

---
+++ src/app/new-wallet/new-wallet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-wallet',
+  templateUrl: './new-wallet.component.html',
+  styleUrls: ['./new-wallet.component.css']
+})
+export class NewWalletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
