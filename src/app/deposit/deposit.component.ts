bc70274984f87c14c6f611f8948e23d40d844731
pankajoodles <pankaj.thakur@oodlestechnologies.com>
2017-09-28 15:06:07 +0530

    deposit qr code access

--- src/app/deposit/deposit.component.ts
+++ src/app/deposit/deposit.component.ts
@@ -1,16 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import {QRCodeComponent} from 'angular2-qrcode';
-
+import { DepositService } from './deposit.service';
+import { DepositeData } from './entity/deposit.entity';
 @Component({
   selector: 'app-deposit',
   templateUrl: './deposit.component.html',
   styleUrls: ['./deposit.component.css'],
+   providers: [DepositService]
 })
 export class DepositComponent implements OnInit {
+   public shortIfo: boolean = false;
+   address="";
+  public coinDataValue:any;
+  public setItemValue:any;
+  public setItem:any=[
+                        { coinValue:"BTC"},
+                        { coinValue:"ETH"},
+                        { coinValue:"BOLENO"}
+                      ];
 
-  constructor() { }
+  constructor( private depositService:DepositService) { }
 
   ngOnInit() {
+    this.setItemValue = "BTC";
+    this.getCoin("BTC");
+  }
+
+  getCoin(data){
+    console.log("select value >>>>>>>>>>>>>>>>>>",data);
+    this.depositService.getCoin(data).subscribe( successData => {
+      let data = successData.data;
+      this.address = data.data.address;
+      
+    },errorData => {
+
+    })
+    
   }
 
 }
+
+
+
---
+++ src/app/deposit/entity/deposit.entity.ts
@@ -0,0 +1,13 @@
+// export class DepositeData {
+//    coinValue:any;
+     
+//     setcoindata(coinValue: string) {
+//         this.coinValue = coinValue;
+//     }
+
+//   constructor() { }
+
+  
+
+
+// }
