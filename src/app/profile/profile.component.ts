ac4cd03c30ff2b100b716cbd891f49dca3fa1b3f
pankajoodles <pankaj.thakur@oodlestechnologies.com>
2017-09-29 15:28:46 +0530

    working on profile page to show and hide during add details and show

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -14,6 +14,7 @@ import { environment } from '../../environments/environment';
   providers: [ProfileService]
 })
 export class ProfileComponent implements OnInit {
+  public isCustomerView:boolean = true;
   public shortIfo: boolean = false;
 
   public getOurBankDetails: any;
@@ -24,7 +25,7 @@ export class ProfileComponent implements OnInit {
   public bankCustomerDetails: any;
 
   public saveButton: boolean = false;
-  public addNewButton: boolean = true;
+  public addNewButton: boolean = false;
   public accounDetails: boolean = false;
   @ViewChild('fileInput') fileInput;
   @ViewChild('profileImage') profileImage;
@@ -255,6 +256,7 @@ export class ProfileComponent implements OnInit {
 
 
   addNew() {
+     this.isCustomerView = false;
     console.log(".........................")
     this.accounDetails = true;
     this.saveButton = true;
@@ -288,9 +290,22 @@ export class ProfileComponent implements OnInit {
   }
 
   customerDetails(customerDetaisForm) {
-    this.profileService.customerBankData(this.bankDetails).subscribe(successData => {
+     this.isCustomerView = true;
       this.accounDetails = false;
 
+      if (this.getOurBankDetails.length === 2) {
+        this.addNewButton = false;
+      
+        return;
+      }
+      else {
+        this.addNewButton = true;
+      }
+     
+   
+    this.profileService.customerBankData(this.bankDetails).subscribe(successData => {
+     
+      this.getUserBankDetails();
     }, errorData => {
 
     })
@@ -306,13 +321,13 @@ export class ProfileComponent implements OnInit {
       let customerDta = this.getOurBankDetails;
 
       if (customerDta.length === 2) {
-         this.addNewButton = true;
+        this.addNewButton = false;
         console.log("array data", customerDta.length);
         return;
       }
-      // else {
-      //   this.addNewButton = true;
-      // }
+      else {
+        this.addNewButton = true;
+      }
 
 
     }, errorData => {
@@ -345,10 +360,10 @@ export class ProfileComponent implements OnInit {
   }
 
   onChange(event) {
-   let files = event.srcElement.files;
-   if(files.length>0) {
-   let fileBrowser = this.profileImage.nativeElement;
-    const formData = new FormData();
+    let files = event.srcElement.files;
+    if (files.length > 0) {
+      let fileBrowser = this.profileImage.nativeElement;
+      const formData = new FormData();
       formData.append("file", fileBrowser.files[0]);
       this.profileService.uploadProfileImage(formData).subscribe(success => {
         console.log(success);
@@ -359,6 +374,6 @@ export class ProfileComponent implements OnInit {
         this.loading = false;
       });
     }
-    
-   }
+
+  }
 }
