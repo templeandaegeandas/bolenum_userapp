c3c5fd3826a43f16b34fac0f9b97d758316259e8
Himanshu <himanshu.kumar@oodlestechnologies.com>
2017-11-15 18:03:47 +0530

    added web socket in withdraw

--- src/app/withdraw/withdraw.component.ts
+++ src/app/withdraw/withdraw.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { WithdrawService } from './withdraw.service';
 import { WithdrawAmount } from './entity/withdraw.entity';
 import { ToastrService } from 'toastr-ng2';
+import { AppEventEmiterService } from '../app.event.emmiter.service';
 
 @Component({
   selector: 'app-withdraw',
@@ -17,10 +18,7 @@ export class WithdrawComponent implements OnInit {
   public balance : any;
   public coinAbbreviation : any;
   loading = false;
-
   txList: any;
-
-
   public beforeLogin: boolean = true;
   public afterLogin: boolean = false;
   options: any;
@@ -29,7 +27,13 @@ export class WithdrawComponent implements OnInit {
   withdrawForm = new WithdrawAmount();
 
 
-  constructor(private withdrawService: WithdrawService, private toastrService: ToastrService) { }
+  constructor(private withdrawService: WithdrawService,private appEventEmiterService:AppEventEmiterService, private toastrService: ToastrService) {
+    this.appEventEmiterService.currentMessage.subscribe(message => {
+      if (message == "WITHDRAW_NOTIFICATION") {
+           this.getListOfUserWithdrawlTransaction();
+      }
+    });
+  }
 
   ngOnInit() {
     this.getCurrencyList();
