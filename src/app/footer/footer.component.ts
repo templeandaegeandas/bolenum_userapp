a88a63da02da501d1dfd26bea0bd697d9f5859b3
Himanshu <himanshu.kumar@oodlestechnologies.com>
2017-12-08 18:47:21 +0530

    added news letter in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes, Router,ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'toastr-ng2';
+import { FooterService } from './footer.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  providers:[FooterService],
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastrService: ToastrService, private router: Router, private activatedRouter: ActivatedRoute ,private footerService:FooterService) { }
 
+  public email: any;
   ngOnInit() {
   }
 
+  notification() {
+    this.footerService.subscribeUser(this.email).subscribe(success => {
+      console.log(success);
+      this.toastrService.success(success.message, 'Success!');
+    },error=> {
+       this.toastrService.error(error.json().message, 'Error!');
+    })
+  }
 }
