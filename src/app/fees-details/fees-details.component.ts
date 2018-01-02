54c1060f8b0a08915aee24e173d3a7ca19e8add0
pankajoodles <pankaj.thakur@oodlestechnologies.com>
2018-01-02 18:04:40 +0530

    working on fees component

--- src/app/fees-details/fees-details.component.ts
+++ src/app/fees-details/fees-details.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FeesService } from './fees-details.service';
 
 @Component({
   selector: 'app-fees-details',
   templateUrl: './fees-details.component.html',
-  styleUrls: ['./fees-details.component.css']
+  styleUrls: ['./fees-details.component.css'],
+  providers: [FeesService]
 })
 export class FeesDetailsComponent implements OnInit {
+  public currencyId: any;
+  public withdrawFee: any;
 
-  constructor() { }
+  constructor(private feesService: FeesService) { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
+   
+   
   }
+
+
+ 
+
 
 }
