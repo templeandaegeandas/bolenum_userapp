8a1275bb7118810eb35760e7304b64a859b97129
Himanshu <himanshu.kumar@oodlestechnologies.com>
2018-01-04 19:53:47 +0530

    integrated notification

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,12 +19,35 @@ export class HeaderComponent implements OnInit {
   token: String;
   fullName: String;
   lastName: String;
+  isLoading: any;
+  hasBlur: any;
+  listOfUserNotification: any;
+  listOfUserNotificationLength: any;
+  public beforeLogin: boolean = true;
+  public afterLogin: boolean = false;
+  jsonMessage: any;
+  countOfUnseeNotification: any;
+
+
   profilePic: String = "assets/images/pic.png";
   constructor(private headerService: HeaderService, private websocketService: WebsocketService, private router: Router, private appEventEmiterService: AppEventEmiterService) {
+    this.isLogIn();
+    if (this.beforeLogin) {
+      websocketService.connectForNonLoggedInUser();
+    }
+    this.appEventEmiterService.currentMessage.subscribe(message => {
+      this.jsonMessage = message;
+
+      if (message == "DOCUMENT_VERIFICATION" || message == "USER_NOTIFICATION" || message == "DEPOSIT_NOTIFICATION" || message == "PAID_NOTIFICATION") {
+        this.getAllUserNotifications();
+      }
+    })
   }
 
   ngOnInit() {
     this.websocketService.connectForLoggedInUser(localStorage.getItem("userId"))
+    this.getAllUserNotifications();
+    this.getCountOfUnseeNotification();
     this.appEventEmiterService.currentMessage.subscribe(message => {
       if (message == "upload") {
         setTimeout(() => {
@@ -58,7 +81,7 @@ export class HeaderComponent implements OnInit {
   }
 
   signOut() {
-      this.headerService.logOut().subscribe(success => {
+    this.headerService.logOut().subscribe(success => {
       this.router.navigate(['login']);
       localStorage.clear();
       this.websocketService.disconnect();
@@ -70,5 +93,41 @@ export class HeaderComponent implements OnInit {
   showDropdown() {
     this.subMenu = !this.subMenu;
   }
+
+  getAllUserNotifications() {
+    this.isLoading = true;
+    this.hasBlur = true;
+    this.headerService.GetUserNotification(1, 5, "createdOn", "desc").subscribe(success => {
+      this.isLoading = false;
+      this.hasBlur = false;
+      this.listOfUserNotification = success.data.content;
+      this.listOfUserNotificationLength = this.listOfUserNotification.length;
+
+
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  isLogIn() {
+
+    if (localStorage.getItem("token") == null) {
+      return;
+    }
+    else {
+      this.beforeLogin = false;
+      this.afterLogin = true;
+
+    }
+  }
+
+  getCountOfUnseeNotification() {
+      this.headerService.getTotalOfUnseeNotification().subscribe(success => {
+      this.isLoading = false;
+      this.hasBlur = false;
+      this.countOfUnseeNotification = success.data;
+    })
+  }
+
 
 }
--- src/app/header/header.service.ts
+++ src/app/header/header.service.ts
@@ -4,10 +4,26 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HeaderService {
+  pageNumber: any;
   constructor(private http: HttpClient) { }
 
   logOut() {
     return this.http.delete('/api/v1/logout')
       .map(res => res.json());
   }
+
+  GetUserNotification(currentPage: number, pageSize: number, sortBy: String, sortOrder: String) {
+    this.pageNumber = currentPage - 1;
+    return this.http.get('/api/v1/user/notification?pageNumber='
+      + this.pageNumber + '&pageSize='
+      + pageSize + '&sortBy='
+      + sortBy + '&sortOrder=' + sortOrder)
+      .map(res => res.json());
+  }
+
+  getTotalOfUnseeNotification() {
+    return this.http.get('/api/v1/user/count/notification')
+    .map(res => res.json());
+    }
+
 }
