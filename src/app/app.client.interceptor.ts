788fd0404d4afe77eb7ffa30d91d002341015fe9
Vishal Kumar <vishal.kumar@oodlestechnologies.com>
2018-02-01 11:35:54 +0530

    session expiry issue fixed

--- src/app/app.client.interceptor.ts
+++ src/app/app.client.interceptor.ts
@@ -1,43 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { Request, XHRBackend, RequestOptions, Response, Http, RequestOptionsArgs, Headers } from '@angular/http';
-import { ToastrService } from 'toastr-ng2';
-import { RouterModule, Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import { ToastrService } from 'toastr-ng2';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
 @Injectable()
-export class HttpClient  extends Http {
+export class HttpClient extends Http {
 
-  constructor(backend: XHRBackend, defaultOptions: RequestOptions,private router:Router , private toastrService: ToastrService, ) 
-  {
+  constructor(backend: XHRBackend, defaultOptions: RequestOptions, private router: Router, private toastrService: ToastrService) {
     super(backend, defaultOptions);
   }
 
   request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+ 
+    if (typeof url === 'string') {
+      if (!options) {
+        options = { headers: new Headers() };
+      }
+      this.setHeaders(options);
+    } else {
+      this.setHeaders(url);
+    }
 
-        if (typeof url === 'string') {
-            if (!options) {
-              options = { headers: new Headers() };
-           }
-            this.setHeaders(options);
-          } else {
-             this.setHeaders(url);
-          }
-
-    return super.request(url, options).catch((error: Response) => {
-        if ((error.status === 401 || error.status === 403 || error.status === 0)) {
-          localStorage.clear();
-          this.router.navigate(['/login']);
-          this.toastrService.info('Session expired! Please login again', 'Info');
-        }
-          return Observable.throw(error);
-        });
+    return super.request(url, options).catch(this.catchErrors());
   }
 
-      private setHeaders(objectToSetHeadersTo: Request | RequestOptionsArgs) {
-        objectToSetHeadersTo.headers.set("Authorization", localStorage.getItem("token"));
-   }
+  private catchErrors() {
 
+    return (res: Response) => {
+        if (res.status === 401 || res.status === 403) {
+          localStorage.clear();
+            //handle authorization errors
+            //in this example I am navigating to login.
+            this.toastrService.info("Error_Session_Expired: redirecting to login!",'Info');
+            window.location.href='#/login'
+        }
+        return Observable.throw(res);
+    };
+  }
 
+  private setHeaders(objectToSetHeadersTo: Request | RequestOptionsArgs) {
+     objectToSetHeadersTo.headers.set("Authorization", localStorage.getItem("token"));
+  }
 }
